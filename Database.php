85b00802fe991141b18e80d22ba9be944c908273
AndrewLaring <larinoffski@gmail.com>
2016-06-05 00:24:10 +0300

    some changes

--- Database.php
+++ Database.php
@@ -2,11 +2,11 @@
 
 class Database extends PDO {
 
-    private $user = 'root';
-    private $pass = 'labuelle';
+    private $user = 'username';
+    private $pass = 'password';
 
     public function __construct() {
-        parent::__construct('mysql' . ':host=localhost;dbname=expert_b6_crm', $this->user, $this->pass);
+        parent::__construct('mysql:host=localhost;dbname=expert_b6_crm', $this->user, $this->pass);
     }
 
 }
